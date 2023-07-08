2ed499ba919aab42992a735730e2a8929975380f
Isaac Bonora <isaac@isbonora.com>
2023-07-08 10:26:47 +0200

    This kinda works.

---
+++ app/components/project/MetaCard.tsx
@@ -0,0 +1,18 @@
+export default function MetaCard({
+    client,
+    year,
+    description,
+    tags,
+  }: {
+    client: string;
+    year: string;
+    description: string;
+    tags: string[];
+  }) {
+    return (
+      <div className="">
+
+     </div>
+    );
+  }
+  
--- app/mdx-components.tsx
+++ app/mdx-components.tsx
@@ -1,30 +1,27 @@
-import { MDXProvider } from "@mdx-js/react";
-import Image from "next/image";
+import type { MDXComponents } from "mdx/types";
+
 // This file allows you to provide custom React components
 // to be used in MDX files. You can import and use any
 // React component you want, including components from
 // other libraries.
 
+import Image from "next/image";
+
 const ResponsiveImage = (props) => (
-    <div>
-        fasdjkfhakjsdhfkj
-  <Image
-    alt={props.alt}
-    sizes="100vw"
-    style={{ width: "100%", height: "auto" }}
-    {...props}
-  />
-  </div>
+    <Image
+      alt={props.alt}
+      sizes="100vw"
+      style={{ width: "100%", height: "auto" }}
+      {...props}
+    />
 );
 
-const components = {
-  img: ResponsiveImage,
-};
-
-export default function Post(props) {
-  return (
-    <MDXProvider components={components}>
-      <main {...props} />
-    </MDXProvider>
-  );
+// This file is required to use MDX in `app` directory.
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    // Allows customizing built-in components, e.g. to add styling.
+    // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
+    img: ResponsiveImage,
+    ...components,
+  };
 }
